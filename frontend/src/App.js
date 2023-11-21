e9034771311c19b23af490dd58102d8f66e17cfc
Graciliana <eagraciliana@gmail.com>
2023-11-21 18:04:32 -0300

    :sparkles: feat: Creating a responsive navigation menu with material ui components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,27 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './components/Home';
 import About from './components/About';
 import Create from './components/Create';
+import NavBar from './components/NavBar';
 
 function App() {
+  const myWidth = 180;
   return (
    <div className = "App">
+   <NavBar 
+   drawerWidth={myWidth}
+   content = {
     <Routes>
       <Route path='' element= {<Home/>}/>
       <Route path='/about' element= {<About/>}/>
       <Route path='/create' element= {<Create/>}/>
     </Routes>
+   
+   }
+   
+   />
+    
    </div>
   );
 }
+
 export default App;
